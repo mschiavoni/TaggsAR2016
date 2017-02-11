31ef00bdf49edbfbeca4ed6aa364a729552acdef
mschiavoni <markdschiavoni@gmail.com>
2017-02-11 03:33:53 -0500

    adding video folder and files
    this includes the video for the background display

--- js/main2.js
+++ js/main2.js
@@ -1,59 +1,75 @@
 
+
 $(document).ready(function() {
 
-renderChart(HHSgrantHist)
+// *****DISCRETIONARY MULTI LEVEL PIE *****
 
-   function renderChart(dataSource){
+FusionCharts.ready(function () {
+  var discretionaryChart = new FusionCharts({
+    "type": 'multilevelpie',
+    "renderAt": 'MLPChartContainer',
+    "id": "mlpchart",
+    "width": '100%',
+    "height": '700',
+    "dataFormat": 'xmlurl',
+    "dataSource": 'xmlFiles/HHSMLPChart.xml'
+  });
+  discretionaryChart.render();
+});
 
-    FusionCharts.ready(function () {
-      var myChart = new FusionCharts({
-        "type": 'mscolumn2d',
-        "renderAt": "HHSgrantHist",
-        "width": "100%",
-        "height": "500",
-        "dataFormat": "xmlurl",
-        "dataSource": "HHSgrantHist.xml"
-      });
 
-      myChart.render();
+// ***** MULTI SELECT GRANT HIST CHART *****
+
+  // On page load, we hardcode the rendering of the first graph in the list.
+  renderChart('HHSgrantHist');
+  /**
+   * function to gather the data and render a chart at a given location.
+   *
+   *  renderChart
+{String} dataSource - both the id of the div and the name of the xml.
+*/
+function renderChart(dataSource){
+  FusionCharts.ready(function () {
+    var myChart = new FusionCharts({
+      "type": 'mscombidy2d',
+      "renderAt": dataSource,
+      "width": "100%",
+      "height": "500",
+      "dataFormat": "xmlurl",
+      "dataSource": "xmlFiles/" + dataSource + ".xml"
     });
+    myChart.render();
+  });
+}
 
-  }
 
+$('#multiSelectDrop').multiselect();
 
-  $('#example-getting-started').multiselect();
-
-  $('.test input[type="checkbox"]').on("change", function(){
-
+  // set function to render graphs on change
+  $('.selectOpt input[type="checkbox"]').on("change", function(){
+    // gather selectedID. This value corresponds to the XML filename as well as the id of the div we render at.
     var selectedID = this.value;
-
-
+    // if event target is checked, render it's corresponding graph.
     if (this.checked) {
       renderChart(selectedID)
-
     }
+    // if it is unchecked, remove it's corresponding graph.
     else {
-     $("#" + selectedID).empty()
-   }
+      $("#" + selectedID).empty()
+    }
+  });
 
 
-   function renderChart(dataSource){
 
-    FusionCharts.ready(function () {
-      var myChart = new FusionCharts({
-        "type": 'mscolumn2d',
-        "renderAt": selectedID,
-        "width": "100%",
-        "height": "500",
-        "dataFormat": "xmlurl",
-        "dataSource": selectedID + ".xml"
-      });
+$('#mapDrop').multiselect();
 
-      myChart.render();
-    });
+  // set function to render graphs on change
+  $('#mapDrop').on("change", function(){
+    var selectedID = this.value.toLowerCase();
+    createMapBox(selectedID)
+  });
 
-  }
-
-
-})
 });
+
+
+
