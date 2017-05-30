af930d98a617d148a2db741150a2436eae537b45
Ashley Smith <ashley22@vt.edu>
2017-05-30 11:29:59 -0400

    update firefox keycode error

--- js/main2.js
+++ js/main2.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 $('.agenciesAgency').keydown(function(e){
    
-   if(e.keyCode == 32){
+   if(e.keyCode === 32){
       e.preventDefault();
        $(".megamenu").css("display", "block")
    } else {
